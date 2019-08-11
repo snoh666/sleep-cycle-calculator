256d2458f0726db82842dff65e3001fad0545ea3
Mikolaj Snochowski <snochopl@gmail.com>
2019-08-11 08:42:01 +0200

    Fixed few typos and added basic structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useSate } from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import Hours from './components/Hours';
+import Minutes from './components/Minutes';
 
 function App() {
   let [hoursToWakeUp, setHoursToWakeUp] = useState(0);
-  let [minutesToWakeUp, setMinutesToWakeUp] = useSate(0);
+  let [minutesToWakeUp, setMinutesToWakeUp] = useState(0);
 
   const updateMinutesToWapeUp = e => {
 
@@ -11,6 +12,15 @@ function App() {
   return (
     <div className="App">
       <h1>Calculate sleep cycles</h1>
+      <div>
+        <Hours />
+        <Minutes />
+      </div>
+      <footer>
+        <div>
+          <span>Created by snoh666</span>
+        </div>
+      </footer>
     </div>
   );
 }
