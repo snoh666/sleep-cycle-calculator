b198163b16fe61ba20cf47e1fa0474c80eda547b
Mikolaj Snochowski <snochopl@gmail.com>
2019-08-15 22:21:23 +0200

    Added calculateMinutes function with showing it

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 function Result({ hours, minutes, Am }) {
 
+  const calculateMinutes = ({hrs, min, am}) => {
+    let result = hrs * 60 + min;
+    return am ? result : result + 12;
+  }
+
   return (
     <div>
       {`Hours: ${hours}, Minutes: ${minutes}, Am: ${Am}`}
+      <br/>
+      {`Whole minutes: ${calculateMinutes({hrs: hours, min: minutes, am: Am})}`}
     </div>
   );
 }
