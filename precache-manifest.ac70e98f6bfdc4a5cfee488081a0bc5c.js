89d970c30b9ef0a42a31e04dd8181678ec918a37
Mikolaj Snochowski <snochopl@gmail.com>
2019-08-18 23:16:57 +0200

    Updates

---
+++ precache-manifest.ac70e98f6bfdc4a5cfee488081a0bc5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424e0bd9c8c6bfadeae23f54b1252c61",
+    "url": "/sleep-cycle-calculator/index.html"
+  },
+  {
+    "revision": "5479e0c0a7ae548b6b2c",
+    "url": "/sleep-cycle-calculator/static/css/main.712fe0c3.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/sleep-cycle-calculator/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "5479e0c0a7ae548b6b2c",
+    "url": "/sleep-cycle-calculator/static/js/main.51ad6182.chunk.js"
+  },
+  {
+    "revision": "86ff4f504f7a229580fb",
+    "url": "/sleep-cycle-calculator/static/js/runtime~main.080d2a1e.js"
+  }
+]);
