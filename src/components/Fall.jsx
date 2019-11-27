44f0af51ef71ec4cd5c656566e319adada6e4c11
Mikolaj Snochowski <snochopl@gmail.com>
2019-11-27 21:42:52 +0100

    Added updateTimeHours function MVP

--- src/components/Fall.jsx
+++ src/components/Fall.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
 
-function FallASleep({ setHours, setMinutes, setAm }) {
+function FallASleep({ setHours, setMinutes, setAm, setForward }) {
 
-  const sleepNow = () => {
-    const time = new Date();
-    let sumTime = time.getMinutes() + time.getHours() * 60;
+  const updateTimeHours = () => {
+    const date = new Date();
+    const nowTime = {
+      hours: date.getHours(),
+      minutes: date.getMinutes() + 20, // Add 20 minutes to have time to fall a sleep
+      isAm: true
+    };
 
-    console.log(sumTime + 540);
+    if(nowTime.hours > 12 && nowTime.hours <= 24) {
+      nowTime.isAm = false;
+      nowTime.hours = Math.floor(nowTime.hours % 12);
+    } else if(nowTime.hours > 24) {
+      nowTime.hours %= 24;
+      if(nowTime.hours > 12 && nowTime.hours <= 24) {
+        nowTime.isAm = false;
+        nowTime.hours = Math.floor(nowTime.hours % 12);
+      }
+    }
 
+    console.log(nowTime);
+
+    setHours(nowTime.hours);
+    setMinutes(nowTime.minutes);
+    setAm(nowTime.isAm);
+    setForward(true);
   }
 
   return (
     <div className="Fall">
-      <button onClick={sleepNow} >Zzz..</button>
+      <button onClick={updateTimeHours} >Zzz..</button>
     </div>
   );
 }
