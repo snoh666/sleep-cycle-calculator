b8cb93a8717a0bfd05420d218eb359a655e2e100
Mikolaj Snochowski <snochopl@gmail.com>
2019-11-27 22:43:21 +0100

    Added ResultWrapper styled component with prop parsing for row reversing

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import TimeStamp from './Timestamp';
 import { getHoursToSleepBack, getHoursToWakeUpAt } from '../functions/getHours';
 
+import ResultWrapper from './styled/ResultWrapper';
+
 function Result({ hours, minutes, Am, isForward }) {
 
   const colors = [
@@ -14,13 +16,13 @@ function Result({ hours, minutes, Am, isForward }) {
   ];
 
   return (
-    <div className="Result">
+    <ResultWrapper reverse={isForward}>
       {(isForward ? getHoursToWakeUpAt(hours, minutes, Am) : getHoursToSleepBack(hours, minutes, Am)).map((el, index) => {
         return (
           <TimeStamp hours={el.hours} minutes={el.minutes} color={colors[index]} key={el.id} />
         );
       })}
-    </div>
+    </ResultWrapper>
   );
 }
 
