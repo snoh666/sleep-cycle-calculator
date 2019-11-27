de5557d2c21b2ca26bfbefa2dae28e505e79c459
Mikolaj Snochowski <snochopl@gmail.com>
2019-11-27 20:22:09 +0100

    Added styled components folder section

---
+++ src/components/styled/AppWrapper.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  text-align: center;
+`;
+
+export default AppWrapper;
