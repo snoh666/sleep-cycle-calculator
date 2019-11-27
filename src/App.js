4f92b4cc66f9dab7663671452de7590f5f06fa4f
Mikolaj Snochowski <snochopl@gmail.com>
2019-11-27 20:49:32 +0100

    Replaced div with class styles with new Styled Components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import Values from './components/Values';
 import Result from './components/Result';
 import FallASleep from './components/Fall';
 
+// Styled Components
+import AppWrapper from './components/styled/AppWrapper';
+import InfoText from './components/styled/InfoText';
+import StyledFooter from './components/styled/StyledFooter';
+
 function App() {
   let [hoursToWakeUp, setHoursToWakeUp] = useState(0);
   let [minutesToWakeUp, setMinutesToWakeUp] = useState(0);
   let [isAm, setIsAm] = useState(true);
 
   return (
-    <div className="App">
+    <AppWrapper>
       <h2>Calculate sleep cycles</h2>
-      <div className="components-box">
+      <div>
         <h4>You need to wake up at:</h4>
         <Values setHours={setHoursToWakeUp} setMinutes={setMinutesToWakeUp} setAm={setIsAm} />
         <hr/>
@@ -19,13 +24,17 @@ function App() {
         <hr/>
         <Result hours={hoursToWakeUp} minutes={minutesToWakeUp} Am={isAm} />
       </div>
-      <div className="info">A good night's sleep consists of 5-6 complete sleep cycles</div>
-      <footer>
+      <InfoText>
+        A good night's sleep consists of 5-6 complete sleep cycles
+      </InfoText>
+      <StyledFooter>
         <div>
-          <span>Created by <a href="https://github.com/snoh666" target="_blank" rel="noopener noreferrer">snoh666</a></span>
+          <span>
+            Created by <a href="https://github.com/snoh666" target="_blank" rel="noopener noreferrer">snoh666</a>
+          </span>
         </div>
-      </footer>
-    </div>
+      </StyledFooter>
+    </AppWrapper>
   );
 }
 
