5fafc933b03915ad08bbf1c253f4f8345b06bef3
Mikolaj Snochowski <snochopl@gmail.com>
2019-11-27 21:43:46 +0100

    Added functions file with getHours functins export

---
+++ src/functions/getHours.js
@@ -0,0 +1,59 @@
+const getHoursToSleepBack = (hrs, min, isAM) => {
+  const dayMinutes = isAM ? hrs * 60 + min : (hrs * 60 + min) + 12 * 60; // Getting time in day elapsed minutes
+
+  let timeStamps = [];
+
+  for(let i = 6; i >= 1; i--) {
+    const timeForTimestamp = Number((dayMinutes - (i === 1 ? 90 : 90 * i) > 0 ?
+      dayMinutes - (i === 1 ? 90 : 90 * i) :
+      dayMinutes - (i === 1 ? 90 : 90 * i) + 1440));
+
+    const hours = Math.floor( timeForTimestamp / 60 );
+
+    timeStamps.push({
+      hours: (hours === 24 ? '00' : hours),
+      minutes: Math.floor(
+        timeForTimestamp
+        % 60
+      ),
+      id: dayMinutes - (
+        i === 1 ? 90 : 90 * i
+      )
+    })
+  }
+
+  return timeStamps;
+}
+
+
+// NOT DONE --------------================-----------------
+const getHoursToWakeUpAt = (hrs, min, isAM) => {
+  const dayMinutes = isAM ? hrs * 60 + min : (hrs * 60 + min) + 12 * 60;;
+
+  let timeStamps = [];
+
+  for(let i = 6; i >= 1; i--) {
+    const timeForTimestamp = Number((dayMinutes + (i === 1 ? 90 : 90 * i) > 0 ?
+      dayMinutes + (i === 1 ? 90 : 90 * i) :
+      dayMinutes + (i === 1 ? 90 : 90 * i) + 1440));
+
+    const hours = Math.floor( timeForTimestamp / 60 ) > 24 ? Math.floor( timeForTimestamp / 60 ) % 24 : Math.floor( timeForTimestamp / 60 );
+
+    timeStamps.push({
+      hours: (hours === 24 ? '00' : hours),
+      minutes: Math.floor(
+        timeForTimestamp
+        % 60
+      ),
+      id: dayMinutes + (
+        i === 1 ? 90 : 90 * i
+      )
+    })
+  }
+
+  return timeStamps;
+}
+
+
+
+export { getHoursToSleepBack, getHoursToWakeUpAt };
