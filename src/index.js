44e7a0215bfa457ec0a7681d9857c33130e2d0a5
Mikolaj Snochowski <snochopl@gmail.com>
2019-11-28 23:06:56 +0100

    Added redux provider and it functionality

--- src/index.js
+++ src/index.js
@@ -3,4 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import { Provider } from 'react-redux';
+import { createStore } from "redux";
+import reducer from './redux/reducer';
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render((
+  <Provider store={store}>
+    <App/>
+  </Provider>
+), document.getElementById('root'));
