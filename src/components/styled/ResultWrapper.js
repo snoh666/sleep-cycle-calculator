5da4b2b5faf4bed95eac8a612c8c16fad1bc1d9a
Mikolaj Snochowski <snochopl@gmail.com>
2019-11-29 18:23:04 +0100

    Added responsive query

--- src/components/styled/ResultWrapper.js
+++ src/components/styled/ResultWrapper.js
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
 
 const ResultWrapper = styled.div`
+  max-width: 500px;
   display: flex;
+  justify-content: space-around;
   flex-direction: ${
     props => props.reverse ? 'row-reverse' : 'row'
   };
   flex-wrap: wrap;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+
 `;
 
 export default ResultWrapper;
