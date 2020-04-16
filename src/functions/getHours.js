fec7501c58b2912ca9b10791b848273855f57fb6
Mikolaj Snochowski <snochopl@gmail.com>
2020-04-16 19:11:22 +0200

    Applied prettier formatting

--- src/functions/getHours.js
+++ src/functions/getHours.js
@@ -1,59 +1,52 @@
 const getHoursToSleepBack = (hrs, min, isAM) => {
-  const dayMinutes = isAM ? hrs * 60 + min : (hrs * 60 + min) + 12 * 60; // Getting time in day elapsed minutes
+  const dayMinutes = isAM ? hrs * 60 + min : hrs * 60 + min + 12 * 60; // Getting time in day elapsed minutes
 
   let timeStamps = [];
 
-  for(let i = 6; i >= 1; i--) {
-    const timeForTimestamp = Number((dayMinutes - (i === 1 ? 90 : 90 * i) > 0 ?
-      dayMinutes - (i === 1 ? 90 : 90 * i) :
-      dayMinutes - (i === 1 ? 90 : 90 * i) + 1440));
+  for (let i = 6; i >= 1; i--) {
+    const timeForTimestamp = Number(
+      dayMinutes - (i === 1 ? 90 : 90 * i) > 0
+        ? dayMinutes - (i === 1 ? 90 : 90 * i)
+        : dayMinutes - (i === 1 ? 90 : 90 * i) + 1440
+    );
 
-    const hours = Math.floor( timeForTimestamp / 60 );
+    const hours = Math.floor(timeForTimestamp / 60);
 
     timeStamps.push({
-      hours: (hours === 24 ? '00' : hours),
-      minutes: Math.floor(
-        timeForTimestamp
-        % 60
-      ),
-      id: dayMinutes - (
-        i === 1 ? 90 : 90 * i
-      )
-    })
+      hours: hours === 24 ? '00' : hours,
+      minutes: Math.floor(timeForTimestamp % 60),
+      id: dayMinutes - (i === 1 ? 90 : 90 * i),
+    });
   }
 
   return timeStamps;
-}
+};
 
-
-// NOT DONE --------------================-----------------
 const getHoursToWakeUpAt = (hrs, min, isAM) => {
-  const dayMinutes = isAM ? hrs * 60 + min : (hrs * 60 + min) + 12 * 60;;
+  const dayMinutes = isAM ? hrs * 60 + min : hrs * 60 + min + 12 * 60;
 
   let timeStamps = [];
 
-  for(let i = 6; i >= 1; i--) {
-    const timeForTimestamp = Number((dayMinutes + (i === 1 ? 90 : 90 * i) > 0 ?
-      dayMinutes + (i === 1 ? 90 : 90 * i) :
-      dayMinutes + (i === 1 ? 90 : 90 * i) + 1440));
+  for (let i = 6; i >= 1; i--) {
+    const timeForTimestamp = Number(
+      dayMinutes + (i === 1 ? 90 : 90 * i) > 0
+        ? dayMinutes + (i === 1 ? 90 : 90 * i)
+        : dayMinutes + (i === 1 ? 90 : 90 * i) + 1440
+    );
 
-    const hours = Math.floor( timeForTimestamp / 60 ) > 24 ? Math.floor( timeForTimestamp / 60 ) % 24 : Math.floor( timeForTimestamp / 60 );
+    const hours =
+      Math.floor(timeForTimestamp / 60) > 24
+        ? Math.floor(timeForTimestamp / 60) % 24
+        : Math.floor(timeForTimestamp / 60);
 
     timeStamps.push({
-      hours: (hours === 24 ? '00' : hours),
-      minutes: Math.floor(
-        timeForTimestamp
-        % 60
-      ),
-      id: dayMinutes + (
-        i === 1 ? 90 : 90 * i
-      )
-    })
+      hours: hours === 24 ? '00' : hours,
+      minutes: Math.floor(timeForTimestamp % 60),
+      id: dayMinutes + (i === 1 ? 90 : 90 * i),
+    });
   }
 
   return timeStamps;
-}
-
-
+};
 
 export { getHoursToSleepBack, getHoursToWakeUpAt };
